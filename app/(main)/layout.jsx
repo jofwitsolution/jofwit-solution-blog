b6ab6d0a9319d8c3acab499cbee03a7eb1be39db
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2024-02-08 21:02:57 +0100

    Add navbar

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/shared/navigation/Navbar";
 import React from "react";
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Navbar />
       <main>{children}</main>
     </div>
   );
---
+++ components/shared/navigation/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white">
+      <div className="max-width h-[3.6rem] flex justify-between items-center">
+        <div>Logo Search</div>
+        <div>Create</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
